4d2e9e2db08987c610fb1551592f8028278367b0
Kris Hill <hillkri@oregonstate.edu>
2020-02-22 07:46:09 +0000

    added new, create, and show routes and views

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var blogSchema = new mongoose.Schema({
 	image: String,
 	body: String,
 	created: {type: Date, default: Date.now}
-})
+});
 
 var Blog = mongoose.model("Blog", blogSchema);
 
@@ -24,6 +24,7 @@ app.get("/", function(req, res){
 	res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err){
@@ -34,6 +35,34 @@ app.get("/blogs", function(req, res){
 	});
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+	//Create Blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		}else{
+			//Redirect back to the index
+			res.redirect("/blogs");
+		}
+	});
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("show", {blog: foundBlog});
+		}
+	})
+});
 
 app.listen(3000, function(){
 	console.log("Now serving the RESTful Blog App!");
