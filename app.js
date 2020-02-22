30b5f694e7b94365d3d176fc359454506abbe380
Kris Hill <hillkri@oregonstate.edu>
2020-02-22 23:08:48 +0000

    added edit route and view, method-override npm module

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express"),
+	methodOverride = require("method-override"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose");
 //APP CONFIG
 mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
 //MONGOOSE MODEL CONFIG
@@ -63,6 +66,28 @@ app.get("/blogs/:id", function(req, res){
 		}
 	})
 });
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("edit", {blog: foundBlog});
+		}
+	})
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+})
 
 app.listen(3000, function(){
 	console.log("Now serving the RESTful Blog App!");
